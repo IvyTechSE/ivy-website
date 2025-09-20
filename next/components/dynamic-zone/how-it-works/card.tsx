ad4ecafaad946d907c2c83e5eeb327f5f5ddc8e0
Joakim Larsson <joakim.larsson@ivytech.se>
2025-09-20 23:03:17 +0200

    refactor: remove Beam component and its styles from the project

--- next/components/dynamic-zone/how-it-works/card.tsx
+++ next/components/dynamic-zone/how-it-works/card.tsx
@@ -11,7 +11,6 @@ import {
 } from 'framer-motion';
 import React, { MouseEvent as ReactMouseEvent, useRef } from 'react';
 
-import Beam from '../../beam';
 import { CanvasRevealEffect } from '../../ui/canvas-reveal-effect';
 
 export const Card = ({
@@ -56,12 +55,6 @@ export const Card = ({
       className="grid grid-cols-1 md:grid-cols-4 max-w-4xl mx-auto py-20"
     >
       <p className="text-9xl font-bold text-neutral-900 mt-8">{'0' + index}</p>
-      <motion.div
-        className="h-px w-full hidden md:block bg-gradient-to-r from-neutral-800 to-neutral-600 rounded-full mt-16 relative overflow-hidden"
-        style={{ width }}
-      >
-        <Beam className="top-0" />
-      </motion.div>
       <div
         className="group p-8 rounded-md border border-neutral-800 bg-neutral-950  relative z-40 col-span-2"
         onMouseMove={handleMouseMove}
