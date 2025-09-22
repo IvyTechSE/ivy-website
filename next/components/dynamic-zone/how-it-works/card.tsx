54bb0fbdbb2173729bc91afaa2c8658fb072f921
Joakim Larsson <joakim.larsson@ivytech.se>
2025-09-22 10:00:35 +0200

    refactor: simplify Card component by removing unused framer-motion effects and CanvasRevealEffect

--- next/components/dynamic-zone/how-it-works/card.tsx
+++ next/components/dynamic-zone/how-it-works/card.tsx
@@ -1,17 +1,6 @@
 'use client';
 
-import {
-  motion,
-  useMotionTemplate,
-  useMotionValue,
-  useMotionValueEvent,
-  useScroll,
-  useSpring,
-  useTransform,
-} from 'framer-motion';
-import React, { MouseEvent as ReactMouseEvent, useRef } from 'react';
-
-import { CanvasRevealEffect } from '../../ui/canvas-reveal-effect';
+import React from 'react';
 
 export const Card = ({
   title,
@@ -22,66 +11,10 @@ export const Card = ({
   description: string;
   index: number;
 }) => {
-  const mouseX = useMotionValue(0);
-  const mouseY = useMotionValue(0);
-
-  function handleMouseMove({
-    currentTarget,
-    clientX,
-    clientY,
-  }: ReactMouseEvent<HTMLDivElement>) {
-    let { left, top } = currentTarget.getBoundingClientRect();
-
-    mouseX.set(clientX - left);
-    mouseY.set(clientY - top);
-  }
-
-  const ref = useRef<HTMLDivElement>(null);
-
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ['end end', 'start start'],
-  });
-
-  const width = useSpring(useTransform(scrollYProgress, [0, 0.2], [0, 300]), {
-    stiffness: 500,
-    damping: 90,
-  });
-
-  useMotionValueEvent(width, 'change', (latest) => {});
   return (
-    <div
-      ref={ref}
-      className="grid grid-cols-1 md:grid-cols-4 max-w-4xl mx-auto py-20"
-    >
+    <div className="grid grid-cols-1 md:grid-cols-4 max-w-4xl mx-auto py-20">
       <p className="text-9xl font-bold text-neutral-900 mt-8">{'0' + index}</p>
-      <div
-        className="group p-8 rounded-md border border-neutral-800 bg-neutral-950  relative z-40 col-span-2"
-        onMouseMove={handleMouseMove}
-      >
-        <motion.div
-          className="pointer-events-none absolute z-10 -inset-px rounded-md opacity-0 transition duration-300 group-hover:opacity-100"
-          style={{
-            maskImage: useMotionTemplate`
-            radial-gradient(
-              350px circle at ${mouseX}px ${mouseY}px,
-              var(--neutral-900),
-              transparent 80%
-            )
-          `,
-          }}
-        >
-          <CanvasRevealEffect
-            animationSpeed={5}
-            containerClassName="bg-transparent absolute inset-0 pointer-events-none"
-            colors={[
-              [59, 130, 246],
-              [139, 92, 246],
-            ]}
-            dotSize={2}
-          />
-        </motion.div>
-
+      <div className="group p-8 rounded-md border border-neutral-800 bg-neutral-950  relative z-40 col-span-2">
         <p className="text-xl font-bold relative z-20 mt-2">{title}</p>
         <p className="text-neutral-400 mt-4 relative z-20">{description}</p>
       </div>
